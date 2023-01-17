1e19e25c91d4594e83e3776b8059e9f4cf43aed9
Justin Trenary <trenaryja@gmail.com>
2023-01-17 00:30:24 -0500

    Add Scaled Text and Hue Slider (#3)
    * add ScaledText component
    
    * add hueslider
    
    * add required props to ScaledText story

---
+++ src/ScaledText/ScaledText.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from 'react'
+
+export type ScaledTextProps = {
+  lines: (
+    | string
+    | {
+        text: string
+        props?: React.SVGProps<SVGTextElement>
+      }
+  )[]
+  props?: React.SVGProps<SVGTextElement>
+}
+
+export const ScaledText = ({ lines, props }: ScaledTextProps) => {
+  const refs = useRef<Array<SVGElement | null>>([])
+
+  useEffect(() => {
+    refs.current = refs.current.slice(0, lines.length)
+  }, [lines])
+
+  useEffect(() => {
+    refs.current.forEach((svg) => {
+      const text = svg?.querySelector('text')
+      const bbox = text?.getBBox()
+      svg?.setAttribute('viewBox', [bbox?.x, bbox?.y, bbox?.width, bbox?.height].join(' '))
+    })
+  }, [lines])
+
+  return (
+    <>
+      {lines.map((line, i) => (
+        <svg ref={(svg) => (refs.current[i] = svg)}>
+          <text {...props} {...(typeof line === 'string' ? undefined : line.props)}>
+            {typeof line === 'string' ? line : line.text}
+          </text>
+        </svg>
+      ))}
+    </>
+  )
+}
