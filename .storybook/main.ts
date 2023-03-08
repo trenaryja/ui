684fe1964618eb7174d5d1490ec779f9f93eea94
Justin Trenary <trenaryja@gmail.com>
2023-03-08 01:25:18 -0500

    bump deps and add storybook types

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,11 @@
-export default {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+import { StorybookConfig } from '@storybook/core-common'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
   framework: '@storybook/react',
-  docs: { docsPage: 'automatic' },
+  features: { postcss: false },
+  logLevel: 'silent',
 }
+
+export default config
