8215452ba1c9bf6e83250bae8cefbf7738b7852a
Justin Trenary <trenaryja@gmail.com>
2023-04-09 01:58:57 -0400

    enhance AsciiImage component/stories

--- src/AsciiImage/AsciiImage.stories.tsx
+++ src/AsciiImage/AsciiImage.stories.tsx
@@ -5,13 +5,30 @@ type Story = StoryObj<typeof AsciiImage>
 const meta: Meta<typeof AsciiImage> = {
   title: 'components/AsciiImage',
   component: AsciiImage,
+  argTypes: {
+    characterRamp: { control: 'text' },
+  },
 }
 export default meta
 
+const src = 'https://source.unsplash.com/random/500x500/?face'
+
 export const Default: Story = {
-  name: 'AsciiImage',
-  render: (args) => {
-    const src = 'https://source.unsplash.com/random/500x500/?face'
-    return <AsciiImage {...args} src={args.src ?? src} />
+  render: (args) => <AsciiImage {...args} />,
+  args: { src },
+}
+
+export const BackgroundClip: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    preStyle: {
+      fontSize: '.4em',
+      backgroundImage: `url(${src})`,
+      backgroundSize: 'cover',
+      backgroundClip: 'text',
+      WebkitBackgroundClip: 'text',
+      color: 'transparent',
+    },
   },
 }
