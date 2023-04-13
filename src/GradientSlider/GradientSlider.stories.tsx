21bb7bd0f94c20b3fbcfe9106b1d2c26c84195f5
Justin Trenary <trenaryja@gmail.com>
2023-04-12 23:29:13 -0400

    improve GradientSlider performance

--- src/GradientSlider/GradientSlider.stories.tsx
+++ src/GradientSlider/GradientSlider.stories.tsx
@@ -3,6 +3,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import chroma from 'chroma-js'
 import { useState } from 'react'
 import { GradientSlider } from '.'
+import { GradientSliderProps } from '../../dist'
 import { HueSlider } from '../HueSlider'
 
 type Story = StoryObj<typeof GradientSlider>
@@ -12,13 +13,15 @@ const meta: Meta<typeof GradientSlider> = {
 }
 export default meta
 
+const defaultArgs: GradientSliderProps = {
+  gradient: ['red', 'green', 'blue'],
+}
+
 export const Default: Story = {
   argTypes: {
     onChange: { table: { disable: true } },
   },
-  args: {
-    gradient: ['red', 'green', 'blue'],
-  },
+  args: defaultArgs,
 }
 
 export const Controlled: Story = {
@@ -28,11 +31,11 @@ export const Controlled: Story = {
     return (
       <Flex direction='column' gap='lg'>
         <h1>Uncontrolled</h1>
-        <GradientSlider {...Default.args} />
+        <GradientSlider {...defaultArgs} />
         <h1>Controlled</h1>
-        <GradientSlider {...Default.args} value={hue} onChange={(x) => setHue(x)} />
+        <GradientSlider {...defaultArgs} value={hue} onChange={(x) => setHue(x)} />
         <h1>Controlled & Locked</h1>
-        <GradientSlider {...Default.args} value={hue} />
+        <GradientSlider {...defaultArgs} value={hue} />
       </Flex>
     )
   },
@@ -41,29 +44,37 @@ export const Controlled: Story = {
 export const ColorPicker: Story = {
   render: () => {
     const [hue, setHue] = useState(0)
-    const [saturation, setSaturation] = useState(100)
-    const [lightness, setLightness] = useState(50)
-    const [alpha, setAlpha] = useState(100)
-    const color = chroma(hue, saturation / 100, lightness / 100, 'hsl').alpha(alpha / 100)
+    const [saturation, setSaturation] = useState(1)
+    const [lightness, setLightness] = useState(0.5)
+    const [alpha, setAlpha] = useState(1)
+    const color = chroma(hue, saturation, lightness, 'hsl').alpha(alpha)
+    const thumbColor = color.hex('rgb')
     return (
       <Flex gap='lg'>
         <Flex direction='column' gap='lg' w='100%' justify='space-between'>
-          <HueSlider value={hue} onChange={(x) => setHue(x)} />
+          <HueSlider thumbColor={thumbColor} value={hue} onChange={(x) => setHue(x)} />
           <GradientSlider
-            gradient={[color.set('hsl.s', 0).hex('rgb'), color.set('hsl.s', 1).hex('rgb')]}
-            value={saturation}
-            onChange={(x) => setSaturation(x)}
+            thumbColor={thumbColor}
+            gradient={[chroma(hue, 0, lightness, 'hsl'), chroma(hue, 1, lightness, 'hsl')]}
+            value={saturation * 100}
+            onChange={(x) => setSaturation(x / 100)}
           />
           <GradientSlider
+            thumbColor={thumbColor}
             gradient={[
-              color.set('hsl.l', 0).hex('rgb'),
-              color.set('hsl.l', 0.5).hex('rgb'),
-              color.set('hsl.l', 1).hex('rgb'),
+              chroma(hue, saturation, 0, 'hsl'),
+              chroma(hue, saturation, 0.5, 'hsl'),
+              chroma(hue, saturation, 1, 'hsl'),
             ]}
-            value={lightness}
-            onChange={(x) => setLightness(x)}
+            value={lightness * 100}
+            onChange={(x) => setLightness(x / 100)}
           />
-          <GradientSlider gradient={[color.alpha(0), color.alpha(1)]} value={alpha} onChange={(x) => setAlpha(x)} />
+          <GradientSlider
+            thumbColor={thumbColor}
+            gradient={['transparent', thumbColor]}
+            value={alpha * 100}
+            onChange={(x) => setAlpha(x / 100)}
+          />
         </Flex>
         <Flex direction='column' w='10em' align='center'>
           <svg viewBox='0 0 1 1'>
--- src/GradientSlider/utils.ts
+++ src/GradientSlider/utils.ts
@@ -1,2 +1,17 @@
-export const sliderGradient = (colors: string[]) =>
-  `linear-gradient(to right, ${colors.map((color) => color).join(', ')})`
+import chroma from 'chroma-js'
+import { ChromaColor } from '../utils'
+
+export const sliderGradient = (colors: chroma.Color[]) =>
+  `linear-gradient(to right, ${colors.map((color) => color.hex()).join(', ')})`
+
+export const getThumbColor = (
+  thumbColor: ChromaColor | undefined,
+  scale: chroma.Scale<chroma.Color> | undefined,
+  scaleValue: number | undefined,
+  defaultColor?: chroma.Color,
+) => {
+  if (thumbColor !== undefined) return chroma(thumbColor)
+  if (scaleValue === undefined) return defaultColor || chroma('white')
+  if (scale === undefined) return chroma('white')
+  return scale(scaleValue)
+}
