e3c9b98ad4ba0c841bf466c13a6c38cdac09931b
Justin Trenary <trenaryja@gmail.com>
2023-10-10 18:56:44 -0400

    convert sliders from mantine to radix

--- src/GradientSlider/GradientSlider.tsx
+++ src/GradientSlider/GradientSlider.tsx
@@ -1,11 +1,14 @@
-import { Slider, SliderProps } from '@mantine/core'
+import * as Slider from '@radix-ui/react-slider'
 import chroma, { Color } from 'chroma-js'
 import { useMemo, useState } from 'react'
 import { ChromaColor } from '../utils'
 import { getThumbColor, sliderGradient } from './utils'
 
-export type GradientSliderProps = Omit<SliderProps, ''> & {
-  showLabel?: boolean
+export type GradientSliderProps = {
+  min?: number
+  max?: number
+  value?: number
+  defaultValue?: number
   gradient: ChromaColor[]
   thumbColor?: ChromaColor
   onChange?: (value: number, color?: Color) => void
@@ -15,16 +18,12 @@ export type GradientSliderProps = Omit<SliderProps, ''> & {
 export const GradientSlider = ({
   onChange,
   includeColorOnChange = false,
-  styles,
-  label,
-  showLabel = false,
   min = 0,
   max = 100,
   value,
   defaultValue,
   gradient,
   thumbColor,
-  ...props
 }: GradientSliderProps) => {
   const gradientColors = useMemo(() => gradient.map((x) => chroma(x)), [gradient])
   const trackBackground = useMemo(() => sliderGradient(gradientColors), [gradientColors])
@@ -46,28 +45,18 @@ export const GradientSlider = ({
   }
 
   return (
-    <Slider
-      label={showLabel ? label : null}
-      value={value}
-      defaultValue={defaultValue}
-      min={min}
-      max={max}
-      styles={{
-        thumb: {
-          borderColor: thumbHex,
-          backgroundColor: thumbHex,
-          ':focus': { outlineColor: thumbHex },
-        },
-        bar: {
-          display: 'none',
-        },
-        track: {
-          '::before': { background: trackBackground },
-        },
-        ...styles,
-      }}
-      {...props}
-      onChange={handleChange}
-    />
+    <div className='grid gap-10'>
+      <Slider.Root
+        value={value === undefined ? value : [value]}
+        defaultValue={defaultValue === undefined ? defaultValue : [defaultValue]}
+        onValueChange={(x) => handleChange(x[0])}
+        min={min}
+        max={max}
+        className='h-4 relative flex items-center select-none touch-none w-full'
+      >
+        <Slider.Track style={{ background: trackBackground }} className='bg-red-500 relative grow rounded-full h-2' />
+        <Slider.Thumb style={{ background: thumbHex }} className='block w-4 h-4 rounded-full focus:outline-none' />
+      </Slider.Root>
+    </div>
   )
 }
