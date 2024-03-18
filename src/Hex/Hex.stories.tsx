4bf651d9344d4e7b471a322c1582eefabdf06722
Justin Trenary <trenaryja@gmail.com>
2024-03-17 23:38:46 -0400

    add Hex component

---
+++ src/Hex/Hex.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Hex } from '..'
+
+type Story = StoryObj<typeof Hex>
+const meta: Meta<typeof Hex> = {
+  title: 'components/Hex',
+  component: Hex,
+}
+export default meta
+
+export const Default: Story = { name: 'Hex', args: { className: 'bg-base-200' } }
+
+export const Nested: Story = {
+  render: (args) => {
+    return (
+      <Hex className='bg-base-200 p-5' {...args}>
+        <Hex className='bg-base-100 w-full' {...args} />
+      </Hex>
+    )
+  },
+}
---
+++ src/Hex/Hex.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps } from 'react'
+import { cn } from '../utils'
+
+export type HexProps = ComponentProps<'div'> & {
+  flat?: boolean
+}
+
+export const Hex = ({ flat, className, children, ...props }: HexProps) => {
+  return (
+    <>
+      <div
+        className={cn(
+          'grid place-items-center overflow-clip',
+          flat
+            ? '[clip-path:polygon(25%_0%,75%_0%,100%_50%,75%_100%,25%_100%,0%_50%)] aspect-[2/sqrt(3)]'
+            : '[clip-path:polygon(50%_0%,100%_25%,100%_75%,50%_100%,0%_75%,0%_25%)] aspect-[sqrt(3)/2]',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    </>
+  )
+}
