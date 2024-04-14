8c10e5f01960d73047c43c01fd66012b6051edb6
Justin Trenary <trenaryja@gmail.com>
2024-04-14 03:33:41 -0400

    migrate to vercel and update configs

--- src/HueSlider/HueSlider.tsx
+++ src/HueSlider/HueSlider.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { GradientSlider, GradientSliderProps } from '..'
 import { ChromaColor } from '../utils'
--- src/ImageAccordion/ImageAccordion.tsx
+++ src/ImageAccordion/ImageAccordion.tsx
@@ -6,40 +6,37 @@ export const ImageAccordion = ({ className, ...props }: React.ComponentProps<typ
   <AccordionPrimitive.Root className={cn('grid gap-2', className)} {...props} />
 )
 
-ImageAccordion.Item = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Item> & { src: string },
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item> & { src: string }
->(({ src, style, className, ...props }, ref) => (
+ImageAccordion.Item = ({
+  src,
+  style,
+  className,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Item> & { src: string }) => (
   <AccordionPrimitive.Item
-    ref={ref}
     style={{ backgroundImage: `url('${src}')`, ...style }}
     className={cn('rounded bg-no-repeat bg-cover bg-center ', className)}
     {...props}
   />
-))
-
-ImageAccordion.Toggle = React.forwardRef<React.ElementRef<'div'>, React.ComponentPropsWithoutRef<'div'>>(
-  ({ children, className, ...props }, ref) => (
-    <div
-      ref={ref}
-      {...props}
-      className={cn('select-none transition-transform duration-200 accordion-toggle px-2', className)}
-    >
-      {children || '▼'}
-    </div>
-  ),
 )
 
-ImageAccordion.Trigger = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {
-    showToggle?: boolean
-    toggleContent?: React.ReactNode
-  }
->(({ toggleContent, showToggle = true, className, children, ...props }, ref) => (
+ImageAccordion.Toggle = ({ children, className, ...props }: React.ComponentProps<'div'>) => (
+  <div {...props} className={cn('select-none transition-transform duration-200 accordion-toggle px-2', className)}>
+    {children || '▼'}
+  </div>
+)
+
+ImageAccordion.Trigger = ({
+  toggleContent,
+  showToggle = true,
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Trigger> & {
+  showToggle?: boolean
+  toggleContent?: React.ReactNode
+}) => (
   <AccordionPrimitive.Header asChild>
     <AccordionPrimitive.Trigger
-      ref={ref}
       className={cn(
         'w-full flex flex-1 items-center justify-between py-4 transition-all [&[data-state=open]_.accordion-toggle]:rotate-180 bg-no-repeat bg-center bg-cover ',
         className,
@@ -50,18 +47,14 @@ ImageAccordion.Trigger = React.forwardRef<
       {showToggle && <ImageAccordion.Toggle>{toggleContent}</ImageAccordion.Toggle>}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
-))
+)
 
-ImageAccordion.Content = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
->(({ className, ...props }, ref) => (
+ImageAccordion.Content = ({ className, ...props }: React.ComponentProps<typeof AccordionPrimitive.Content>) => (
   <AccordionPrimitive.Content
-    ref={ref}
     className={cn(
       'overflow-hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
       className,
     )}
     {...props}
   />
-))
+)
