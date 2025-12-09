e7d20798b1e16e36be02b0ae32b7ab9def00bda2
Justin Trenary <trenaryja@gmail.com>
2025-12-09 18:29:24 -0500

    export isDark utility

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,19 @@
-import type { DaisyThemeColor, TailwindBreakpointName, TailwindColor, TailwindColorName, TailwindIndex } from '@/types'
+import type { DaisyThemeColor, TailwindBreakpointName, TailwindColor } from '@/types'
 import { daisyThemes } from '@/types'
 import chroma from 'chroma-js'
 import * as R from 'remeda'
 import defaultTheme from 'tailwindcss/defaultTheme'
+
+export const addOpacityToOklch = (oklch: string | undefined, opacity: number) =>
+	`${oklch?.split(')')[0]} / ${opacity / 100})`
+
+export const isDark = (color: string) => {
+	const c = chroma(color)
+	if (!chroma.valid(c)) return false
+	const contrastW = chroma.contrast(c, '#fff')
+	const contrastB = chroma.contrast(c, '#000')
+	return contrastW > contrastB
+}
 
 export const daisyThemeColors = R.keys(daisyThemes.dark)
 	.filter((key) => key.startsWith('--color-'))
@@ -22,37 +33,12 @@ export const breakpoints = R.pipe(
 
 export const colors = R.omit(defaultTheme.colors(), ['inherit', 'current', 'transparent', 'black', 'white'])
 
-export const palette = R.pipe(
-	colors,
-	R.entries(),
-	R.map(([name, shades]) => ({
-		name: name satisfies TailwindColorName,
-		shades: R.pipe(
-			shades,
-			R.entries(),
-			R.map(([index, oklch]) => ({
-				oklch,
-				index: index satisfies TailwindIndex,
-				hex: chroma(oklch).hex(),
-				fullName: `${name}-${index}` satisfies TailwindColor,
-				isDark: chroma.contrast(oklch ?? '', 'white') > chroma.contrast(oklch ?? '', 'black'),
-			})),
-		),
+export const palette = R.flatMap(R.entries(colors), ([baseName, shades]) =>
+	R.map(R.entries(shades), ([index, oklch]) => ({
+		baseName,
+		index,
+		oklch,
+		fullName: `${baseName}-${index}` satisfies TailwindColor,
+		isDark: isDark(oklch),
 	})),
 )
-
-export const flatPalette = palette.flatMap((color) => color.shades.map((shade) => ({ baseName: color.name, ...shade })))
-
-export const getTailwindOklch = (color: 'random' | TailwindColor, index?: TailwindIndex): string | undefined => {
-	if (color === 'random') {
-		const randomColor = flatPalette[Math.floor(Math.random() * flatPalette.length)]!
-		if (index === undefined) return randomColor.oklch
-		return getTailwindOklch(`${randomColor.baseName}-${index}`)
-	}
-
-	const [name, i] = color.split('-') as [TailwindColorName, TailwindIndex]
-	return colors[name][i]
-}
-
-export const addOpacityToOklch = (oklch: string | undefined, opacity: number) =>
-	`${oklch?.split(')')[0]} / ${opacity / 100})`
