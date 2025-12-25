cd10c4b77672773e841cb92da8156639aaccd115
Justin Trenary <trenaryja@gmail.com>
2025-12-25 02:35:20 -0500

    feat: add useBreakpoint hook

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,17 @@
 import type { colors, palette } from '@/utils'
 import daisyThemes from 'daisyui/theme/object'
-import type defaultTheme from 'tailwindcss/defaultTheme'
 import type { OmitIndexSignature } from 'type-fest'
+
+export { daisyThemes }
 
 export type Suggest<T, U> = (U & { __?: never }) | T
 
-export { daisyThemes }
 export type DaisyThemeName = keyof OmitIndexSignature<typeof daisyThemes>
 export type DaisyTheme = (typeof daisyThemes)[DaisyThemeName]
 export type DaisyThemeColor = {
 	[K in keyof DaisyTheme]: K extends `--color-${infer Color}` ? Color : never
 }[keyof DaisyTheme]
 
-export type TailwindBreakpointName = keyof typeof defaultTheme.screens
 export type TailwindColorName = keyof typeof colors
 export type TailwindShadeDictionary = (typeof colors)[TailwindColorName]
 export type TailwindIndex = keyof TailwindShadeDictionary
