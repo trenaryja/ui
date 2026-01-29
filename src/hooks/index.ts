828b2a4fb345e0bcb02aed72489dff83fd94a15b
Justin Trenary <trenaryja@gmail.com>
2026-01-28 20:35:04 -0500

    refactor(theme): move theme to hooks as useTheme

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,9 @@
-export { useTheme } from '../lib/theme'
-
+export * from './use-ascii/use-ascii'
 export * from './use-breakpoint/use-breakpoint'
 export * from './use-cycle/use-cycle'
 export * from './use-duration/use-duration'
 export * from './use-native-dialog/use-native-dialog'
 export * from './use-native-popover/use-native-popover'
 export * from './use-radio-group/use-radio-group'
+export * from './use-theme/use-theme'
 export * from './use-unsaved-changes-prompt/use-unsaved-changes-prompt'
